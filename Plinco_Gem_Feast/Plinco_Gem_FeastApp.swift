d4d76c8f8c3c25894615d3bea1bd46f5a5f19aeb
Alexander <oBelyash@yandex.ru>
2025-02-13 05:06:36 +0300

    Initial Commit

---
+++ Plinco_Gem_Feast/Plinco_Gem_FeastApp.swift
@@ -0,0 +1,17 @@
+//
+//  Plinco_Gem_FeastApp.swift
+//  Plinco_Gem_Feast
+//
+//  Created by Александр Андреев on 13.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Plinco_Gem_FeastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
